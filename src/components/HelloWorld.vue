1de05458fe2af2d2d50044fd70cac653e175c442
g7834 <wowdacom.hu@housefun.com.tw>
2022-11-10 08:45:24 +0000

    初步整理

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,80 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import SINGERS_INVOICES from "../assets/singerInvoice.json";
+import SINGERS from "../assets/singerList.json";
+
+defineProps({
+  msg: String,
+});
+
+const alert = ref("");
+
+const singer_statement = (invoice, singer) => {
+  let msg = "";
+  let originalTotalAmount = 0;
+  let totalAmount = 0;
+  let totalDiscount = 0;
+
+  for (let ticket of invoice.performances) {
+    let cost = 0;
+    let save = 0;
+    let off = 1;
+    switch (singer[ticket.playID].type) {
+      case "concert":
+        off = 0.9;
+        cost = ticket.fee * off;
+        save = ticket.fee - cost;
+        break;
+      case "exhibition":
+        off = 0.5;
+        cost = ticket.fee * off;
+        save = ticket.fee - cost;
+        break;
+      default:
+        throw new Error(`unkown type: ${singer[ticket.playID].type}`);
+    }
+
+    originalTotalAmount += ticket.fee;
+    totalAmount += cost;
+    totalDiscount += save;
+  }
+
+  msg += `${
+    invoice.customer
+  } 您好：\n總票價為 <span style="text-decoration:line-through">${originalTotalAmount.toLocaleString(
+    "en-US"
+  )}$</span>\n`;
+  msg += `本次總消費為 ${totalAmount.toLocaleString("en-US")}$\n`;
+  msg += `此次會員資格為您節省了 ${totalDiscount.toLocaleString("en-US")}$\n`;
+  return msg;
+};
+
+onMounted(() => {
+  const message = singer_statement(SINGERS_INVOICES[0], SINGERS);
+  // Try edit me
+  console.log(message);
+
+  // Update header text
+  alert.value = message;
+});
+</script>
+
+<template>
+  <p>2022 年底前申辦會員，享有演唱會九折優惠，一般展覽一律半價，敬啟把握!</p>
+  <h1 v-html="alert" id="header"></h1>
+</template>
+
+<style scoped>
+h1 {
+  background: transparent;
+  color: #fcbe24;
+  padding: 0 24px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  flex-direction: column;
+  white-space: pre-wrap;
+}
+</style>
